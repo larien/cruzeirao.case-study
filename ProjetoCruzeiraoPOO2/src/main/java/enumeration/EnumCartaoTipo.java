3866daf5f2e2222fb516f55d916aaca17d20a572
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2018-04-16 18:20:59 -0300

    qwe

---
+++ ProjetoCruzeiraoPOO2/src/main/java/enumeration/EnumCartaoTipo.java
@@ -0,0 +1,5 @@
+package enumeration;
+
+public enum EnumCartaoTipo {
+vermelho, verde, amarelo, unicornio, azul
+}
---
+++ ProjetoCruzeiraoPOO2/src/main/java/enumeration/EnumSexoTipo.java
@@ -0,0 +1,5 @@
+package enumeration;
+
+public enum EnumSexoTipo {
+Feminino, Masculino, Indefinido
+}
